4e67f82aa0131d718a510bd0ac640a37242f3cfb
munozvicente <vanm@outlook.cl>
2026-01-14 19:32:46 -0300

    chore(config): carga OXR_APP_ID (OpenExchangeRates App ID) desde .env

--- xpendit_backend/xpendit_backend/settings.py
+++ xpendit_backend/xpendit_backend/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Take environment variables from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -122,3 +126,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# OpenExchangeRates App ID access
+OXR_APP_ID = os.getenv("OXR_APP_ID")
