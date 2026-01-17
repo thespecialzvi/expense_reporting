1e954f1573af57f523784c1a35b9668eaf01b477
Zvi Moller <vicente.a.munoz@proton.me>
2026-01-17 20:27:01 -0300

    minor fixes

--- xpendit_backend/analyze.py
+++ xpendit_backend/analyze.py
@@ -47,7 +47,6 @@ elif (HERE / "backend" / "engine").exists():
 from engine.models import Empleado, Gasto
 from engine.validator import validar_gasto
 
-
 OXR_BASE_URL = "https://openexchangerates.org/api"
 DEFAULT_ANALYSIS_MD = "../output/ANALISIS.md"
 DEFAULT_CSV_NAME = "../staticfiles/gastos_historicos.csv"
